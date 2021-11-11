b5e58ecb24f62ae542d4dc207f4cb4dc377feacc
whutkanido <flagranthusk@gmail.com>
2021-11-11 17:18:48 -0500

    cleaned up a bit

--- s3-public-access-remediation.py
+++ s3-public-access-remediation.py
@@ -16,28 +16,26 @@ all_buckets = list(buckets['Name'] for buckets in response['Buckets'])
 for each_bucket in all_buckets:
     try:
         result = s3.get_bucket_policy(Bucket=each_bucket)
-    
     except botocore.exceptions.ClientError as error:
-         print(f"\nNo policy is attached to {each_bucket}")
-         pass
+        print(f"\nNo policy is attached to {each_bucket}")
+        pass
 
 
     # some funky stuff you have to do to format the returned object properly
     parsed = json.loads(result['Policy'])
     # print(json.dumps(parsed, indent=4, sort_keys=True))
-    shorthand = parsed['Statement'][0]
+    policy = parsed['Statement'][0]
 
     # if the bucket allows public access
     # let user know it is vulnerable
     # then automatically remediate by blocking public access
- 
-    if (shorthand['Effect'] == "Allow"
-        and shorthand['Principal'] == "*" 
-        and shorthand['Action'] == "s3:GetObject"):
-            print(f"\nResource {shorthand['Resource']} is vulnerable")
-            print(f"Because {shorthand['Action']}")
-            print(f"is {shorthand['Effect']}ed for Principal: {shorthand['Principal']}")
-            print("are enabled")
+
+    if (policy['Effect'] == "Allow"
+        and policy['Principal'] == "*" 
+        and policy['Action'] == "s3:GetObject"):
+            print(f"\nResource {policy['Resource']} is vulnerable")
+            print(f"Because {policy['Action']}")
+            print(f"is {policy['Effect']}ed for Principal: {policy['Principal']}")
 
             fixed_policy = {
             "Version": "2012-10-17",
